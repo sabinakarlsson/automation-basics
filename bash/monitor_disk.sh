a388da10583224fa13e1f197219566070d37b19e
sabinakarlsson <karlsson.sst@gmail.com>
2026-01-09 07:53:30 +0100

    Update monitoring and logging scripts

--- bash/monitor_disk.sh
+++ bash/monitor_disk.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 THRESHOLD=80
+LOGFILE="../python/system_status.log"
 
 df -h | grep '^/dev/' | while read fs size used avail percent mount; do
     usage=${percent%\%}
+
+    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
     
     if [ "$usage" -ge "$THRESHOLD" ]; then
-    echo "WARNING: $fs on $mount is at ${usage}% usage"
+    echo "$TIMESTAMP WARNING: $fs on $mount is at ${usage}% usage"
+    echo "$TIMESTAMP WARNING: $fs on $mount is at ${usage}% usage" >> "$LOGFILE"
+    else
+    echo "$TIMESTAMP $fs on $mount is at ${usage}% usage"
+    echo "$TIMESTAMP $fs on $mount is at ${usage}% usage" >> "$LOGFILE"
     fi
 
 done
